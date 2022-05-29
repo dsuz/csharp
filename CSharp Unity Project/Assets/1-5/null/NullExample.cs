1e246eae62a05ea1aafe3cdc425bd79f14e68a2e
D. Suzuki <bboydaisuke@gmail.com>
2022-05-29 21:04:39 +0900

    add example for null

---
+++ CSharp Unity Project/Assets/1-5/null/NullExample.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+/// <summary>
+/// null の説明として、Sprite の色を変える。
+/// </summary>
+public class NullExample : MonoBehaviour
+{
+    /// <summary>変える先の色</summary>
+    [SerializeField] Color _color = Color.red;
+
+    /// <summary>
+    /// Sprite の色を変える。外部から呼び出して使う。
+    /// </summary>
+    public void ChangeColor()
+    {
+        // SpriteRenderer コンポーネントを取得する。追加されてない場合は null が戻される。
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = _color;
+    }
+
+    /// <summary>
+    /// Sprite の色を変える。外部から呼び出して使う。
+    /// エラーにならないように null チェックをしている。
+    /// </summary>
+    public void ChangeColorWithNullCheck()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // null チェック
+        if (spriteRenderer != null)
+        {
+            // null じゃなければ色を変える
+            spriteRenderer.color = _color;
+        }
+        else
+        {
+            // null だったらインスタンスを操作せず、警告を出力する
+            Debug.LogWarning($"{name} に Sprite Renderer コンポーネントが追加されていません");
+        }
+    }
+}
