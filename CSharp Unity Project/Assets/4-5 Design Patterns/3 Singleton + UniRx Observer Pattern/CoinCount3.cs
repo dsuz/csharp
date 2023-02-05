9c8c3c256c661f51e0932c864cc7ee449229e279
D. Suzuki <bboydaisuke@gmail.com>
2023-02-06 01:04:20 +0900

    １つのシーンでカメラで遊んでみた。コメントを修正し、パッケージも更新した。

--- CSharp Unity Project/Assets/4-5 Design Patterns/3 Singleton + UniRx Observer Pattern/CoinCount3.cs
+++ CSharp Unity Project/Assets/4-5 Design Patterns/3 Singleton + UniRx Observer Pattern/CoinCount3.cs
@@ -15,6 +15,6 @@ public class CoinCount3 : MonoBehaviour
         _text = GetComponent<Text>();
         _text.text = "0";
         GameManager3.Instance.ObserveEveryValueChanged(gm => gm.CoinCount)  // ここでプロパティの監視を設定している
-            .Subscribe(coinCount => _text.text = coinCount.ToString());     // ここで条件を満たした時に実行する処理を登録している
+            .Subscribe(coinCount => _text.text = coinCount.ToString());     // ここで監視対象の値が変更された時に実行する処理を登録している
     }
 }
